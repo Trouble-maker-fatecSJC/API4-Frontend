43300dd4fa79bc4e401996fbbd3fb461ddf3923a
ClaudioJayme <claudiojayme@gmail.com>
2025-03-19 23:25:07 -0300

    style: centralizando e ajustando detalhe estacao.

--- app/src/components/estacao/DetalheEstacao.tsx
+++ app/src/components/estacao/DetalheEstacao.tsx
@@ -1,4 +1,71 @@
-import React from 'react';
+// interface Estacao {
+//   nome: string;
+//   latitude: number;
+//   longitude: number;
+//   endereco: string;
+//   parametros: string[];
+// }
+
+// export default function DetalheEstacao({ estacao }: { estacao: Estacao }) {
+//   return (
+//     <div className="overflow-y-auto overflow-x-hidden 
+//     fixed top-0 right-0 left-0 z-50  items-center justify-center 
+//     flex-col max-w-md mx-5 md:mx-auto mt-10 bg-white rounded-lg overflow-hidden shadow-2xl">
+      
+//       <div className="relative text-2xl py-4 px-6 bg-gray-900 text-white text-center font-bold uppercase">
+      
+//           <button  className=" absolute top-0 right-1 btn-close p-1 ">
+//               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-6 w-6">
+//                 <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+//               </svg>
+//             </button>
+  
+//         Detalhes da Estação
+        
+//       </div>
+
+//       <div className="py-4 px-6">
+//         <div className="mb-4">
+//           <h2 className="text-gray-700 font-bold mb-2">Nome:</h2>
+//           <p>{estacao.nome}</p>
+//         </div>
+
+//         <div className="mb-4">
+//           <h2 className="text-gray-700 font-bold mb-2">Latitude:</h2>
+//           <p>{estacao.latitude}</p>
+//         </div>
+
+//         <div className="mb-4">
+//           <h2 className="text-gray-700 font-bold mb-2">Longitude:</h2>
+//           <p>{estacao.longitude}</p>
+//         </div>
+
+//         <div className="mb-4">
+//           <h2 className="text-gray-700 font-bold mb-2">Endereço:</h2>
+//           <p>{estacao.endereco}</p>
+//         </div>
+
+//         <div className="mb-4">
+//           <h2 className="text-gray-700 font-bold mb-2">Parâmetros:</h2>
+//           <ul>
+//             {estacao.parametros.map((parametro, index) => (
+//               <li key={index}>{parametro}</li>
+//             ))}
+//           </ul>
+//         </div>
+//         <div className="mb-4 flex justify-center gap-4">
+//           <button className="btn-editar">
+//             Editar
+//           </button>
+//           <button className="btn-delete">
+//             Deletar
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
 
 interface Estacao {
   nome: string;
@@ -8,20 +75,25 @@ interface Estacao {
   parametros: string[];
 }
 
-export default function DetalheEstacao({ estacao }: { estacao: Estacao }) {
+interface DetalheEstacaoProps {
+  estacao: Estacao;
+  onClose: () => void; // Função de fechamento
+}
+
+export default function DetalheEstacao({ estacao, onClose }: DetalheEstacaoProps) {
   return (
-    <div className="max-w-md mx-auto mt-10 bg-white shadow-lg rounded-lg overflow-hidden">
+    <div className="overflow-y-auto overflow-x-hidden 
+    fixed top-0 right-0 left-0 z-50  items-center justify-center 
+    flex-col max-w-md mx-5 md:mx-auto mt-10 bg-white rounded-lg overflow-hidden shadow-2xl">
       
+      <div className="relative text-2xl py-4 px-6 bg-gray-900 text-white text-center font-bold uppercase">
       
-      
-      <div className="text-2xl py-4 px-6 bg-gray-900 text-white text-center font-bold uppercase">
-      <div className="w-20 mx-auto">
-          <button  className="rounded-lg text-gray-700 transition-all hover:bg-gray-100 bg-gray-200 p-1">
+          <button onClick={onClose} className=" absolute top-0 right-1 btn-close p-1 ">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-6 w-6">
                 <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
               </svg>
             </button>
-          </div>
+  
         Detalhes da Estação
         
       </div>
--- app/src/components/estacao/Estacoes.tsx
+++ app/src/components/estacao/Estacoes.tsx
@@ -1,8 +1,90 @@
+// import { useState, useEffect } from 'react';
+// import DetalheEstacao from './DetalheEstacao'; // Importando o componente de detalhes
+
+// export default function Estacoes() {
+//   // Simulando a resposta da API com uma lista de estações
+//   interface Estacao {
+//     nome: string;
+//     latitude: number;
+//     longitude: number;
+//     endereco: string;
+//     parametros: string[];
+//   }
+
+//   const [estacoes, setEstacoes] = useState<Estacao[]>([]);
+//   const [estacaoSelecionada, setEstacaoSelecionada] = useState<Estacao | null>(null);
+
+//   // Aqui, você faria a requisição para buscar a lista de estações da sua API usando useEffect
+//   useEffect(() => {
+//     // Simulação de fetch da API
+//     const fetchEstacoes = async () => {
+//       const response = await new Promise<Estacao[]>((resolve) => {
+//         setTimeout(() => {
+//           resolve([
+//             {
+//               nome: "Estação Central",
+//               latitude: -23.5505,
+//               longitude: -46.6333,
+//               endereco: "Av. Paulista, 1000, São Paulo, SP",
+//               parametros: ["1", "2", "3"]
+//             },
+//             {
+//               nome: "Estação Norte",
+//               latitude: -23.5636,
+//               longitude: -46.6565,
+//               endereco: "Rua do Norte, 200, São Paulo, SP",
+//               parametros: ["4", "5", "6"]
+//             },
+//           ]);
+//         }, 1000);
+//       });
+
+//       setEstacoes(response);
+//     };
+
+//     fetchEstacoes();
+//   }, []);
+
+//   // Função para exibir os detalhes de uma estação
+//   const mostrarDetalhes = (estacao: Estacao) => {
+//     setEstacaoSelecionada(estacao);
+//   };
+
+//   return (
+//     <div className="flex sm:flex-row gap-4 bg-gray-800 items-center flex-col">
+//       {/* Exibe a lista de estações */}
+//       {estacoes.map((estacao, index) => (
+//         <div key={index} className="relative bg-gray-900 block p-6 border border-gray-100 rounded-lg w-[300px] mt-4">
+//           <span className="absolute inset-x-0 bottom-0 h-2 bg-gradient-to-r from-green-300 via-green-500 to-green-600"></span>
+
+//           <div className="my-4">
+//             <h2 className="text-white text-2xl font-bold pb-2">{estacao.nome}</h2>
+//             <p className="text-gray-300 py-1">Latitude: {estacao.latitude}</p>
+//             <p className="text-gray-300 py-1">Longitude: {estacao.longitude}</p>
+//           </div>
+
+//           <div className="flex justify-end">
+//             <button
+//               onClick={() => mostrarDetalhes(estacao)}
+//               className="btn-detalhe"
+//             >
+//               Detalhe
+//             </button>
+//           </div>
+//         </div>
+//       ))}
+
+//       {/* Se houver uma estação selecionada, exibe o componente de detalhes */}
+//       {estacaoSelecionada && <DetalheEstacao estacao={estacaoSelecionada} />}
+//     </div>
+//   );
+// }
+
+
 import { useState, useEffect } from 'react';
 import DetalheEstacao from './DetalheEstacao'; // Importando o componente de detalhes
 
 export default function Estacoes() {
-  // Simulando a resposta da API com uma lista de estações
   interface Estacao {
     nome: string;
     latitude: number;
@@ -14,9 +96,7 @@ export default function Estacoes() {
   const [estacoes, setEstacoes] = useState<Estacao[]>([]);
   const [estacaoSelecionada, setEstacaoSelecionada] = useState<Estacao | null>(null);
 
-  // Aqui, você faria a requisição para buscar a lista de estações da sua API usando useEffect
   useEffect(() => {
-    // Simulação de fetch da API
     const fetchEstacoes = async () => {
       const response = await new Promise<Estacao[]>((resolve) => {
         setTimeout(() => {
@@ -45,14 +125,17 @@ export default function Estacoes() {
     fetchEstacoes();
   }, []);
 
-  // Função para exibir os detalhes de uma estação
   const mostrarDetalhes = (estacao: Estacao) => {
     setEstacaoSelecionada(estacao);
   };
 
+  // Função para fechar os detalhes
+  const fecharDetalhes = () => {
+    setEstacaoSelecionada(null);
+  };
+
   return (
     <div className="flex sm:flex-row gap-4 bg-gray-800 items-center flex-col">
-      {/* Exibe a lista de estações */}
       {estacoes.map((estacao, index) => (
         <div key={index} className="relative bg-gray-900 block p-6 border border-gray-100 rounded-lg w-[300px] mt-4">
           <span className="absolute inset-x-0 bottom-0 h-2 bg-gradient-to-r from-green-300 via-green-500 to-green-600"></span>
@@ -75,7 +158,7 @@ export default function Estacoes() {
       ))}
 
       {/* Se houver uma estação selecionada, exibe o componente de detalhes */}
-      {estacaoSelecionada && <DetalheEstacao estacao={estacaoSelecionada} />}
+      {estacaoSelecionada && <DetalheEstacao estacao={estacaoSelecionada} onClose={fecharDetalhes} />}
     </div>
   );
 }
