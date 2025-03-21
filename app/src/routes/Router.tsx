b1e44fd530d8f9edc1e105e4534837bd2c8cd1b7
Claudio Jayme <142222453+ClaudioJaymeDiniz@users.noreply.github.com>
2025-03-21 08:37:19 -0300

    feat: adicionando header-adm

--- app/src/routes/Router.tsx
+++ app/src/routes/Router.tsx
@@ -3,6 +3,9 @@ import Home from "../pages/home";
 import Login from "../pages/login";
 import PaginaNaoEncontrada from "../pages/paginaNaoEncontrada";
 import AdmHome from "../pages/adm";
+import Estacoes from "../components/estacao/Estacoes";
+import CadastroEstacao from "../components/estacao/CadastroEstacao";
+// import EditarEstacao from "../components/estacao/EditarEstacao";
 
 export default function AppRoutes(){
     return (
@@ -10,6 +13,10 @@ export default function AppRoutes(){
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/adm" element={<AdmHome />} />
+        <Route path="/estacoes" element={<Estacoes />} />
+        <Route path="/cadastrestacao" element={<CadastroEstacao />} />
+        
+
         <Route path="*" element={<PaginaNaoEncontrada />} /> 
         {/* Rota para página 404 */}
       </Routes>
