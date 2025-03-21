f4497afc299c5f17323f476db0c0e57b62979d46
Claudio Jayme <142222453+ClaudioJaymeDiniz@users.noreply.github.com>
2025-03-21 08:48:06 -0300

    feat: teste crud estacao com back

--- app/src/components/estacao/CadastroEstacao.tsx
+++ app/src/components/estacao/CadastroEstacao.tsx
@@ -1,27 +1,80 @@
 import React, { useState } from 'react';
+import Header from '../header/Header';
 
 export default function CadastroEstacao() {
-  // Estado para armazenar os serviços selecionados
-  const [servicos, setServicos] = useState(['']);
+  // Estado para armazenar os dados do formulário
+  const [nome, setNome] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+  const [dataInstalacao, setDataInstalacao] = useState('');
+  const [tipoEstacao, setTipoEstacao] = useState('');
+  const [indicativoAtiva, setIndicativoAtiva] = useState(false);
+  const [estacao, setEstacao] = useState(['']);
 
   // Função para adicionar um novo campo de serviço
   const adicionarServico = () => {
-    setServicos([...servicos, '']);
+    setEstacao([...estacao, '']);
   };
 
   // Função para lidar com a mudança de valor de cada campo de seleção
-  const handleServicoChange = (index: number, event: React.ChangeEvent<HTMLSelectElement>) => {
-    const novosServicos = [...servicos];
-    novosServicos[index] = event.target.value;
-    setServicos(novosServicos);
+  const handleEstacaoChange = (index: number, event: React.ChangeEvent<HTMLSelectElement>) => {
+    const novaEstacao = [...estacao];
+    novaEstacao[index] = event.target.value;
+    setEstacao(novaEstacao);
+  };
+
+  // Função para enviar os dados para o back-end
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // Criar o objeto com os dados do formulário
+    const dadosEstacao = {
+      Nome: nome,
+      Latitude: parseFloat(latitude),
+      Longitude: parseFloat(longitude),
+      Data_Instalacao: new Date(dataInstalacao), // Converte a data para o formato correto
+      Tipo_Estacao: tipoEstacao,
+      Indicativo_Ativa: indicativoAtiva,
+      Servicos: estacao, // Enviar os parâmetros selecionados
+    };
+
+    try {
+      // Enviar a requisição para a API
+      const response = await fetch('http://localhost:8800/estacao/cadastrar-estacao', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dadosEstacao), // Envia os dados no formato JSON
+      });
+
+      if (response.ok) {
+        alert('Estação cadastrada com sucesso!');
+        // Limpar os campos do formulário após o envio
+        setNome('');
+        setLatitude('');
+        setLongitude('');
+        setDataInstalacao('');
+        setTipoEstacao('');
+        setIndicativoAtiva(false);
+        setEstacao(['']);
+      } else {
+        alert('Erro ao cadastrar a estação');
+      }
+    } catch (error) {
+      alert('Erro ao conectar com o servidor');
+      console.error(error);
+    }
   };
 
   return (
+    <>
+    <Header />
     <div className="max-w-md mx-auto mt-10 bg-white shadow-lg rounded-lg overflow-hidden">
       <div className="text-2xl py-4 px-6 bg-gray-900 text-white text-center font-bold uppercase">
         Estação
       </div>
-      <form className="py-4 px-6" action="" method="POST">
+      <form className="py-4 px-6" onSubmit={handleSubmit}>
         {/* Campo Nome */}
         <div className="mb-4">
           <label className="block text-gray-700 font-bold mb-2" htmlFor="name">
@@ -32,19 +85,8 @@ export default function CadastroEstacao() {
             id="name"
             type="text"
             placeholder="Nome estação"
-          />
-        </div>
-
-        {/* Campo Endereço */}
-        <div className="mb-4">
-          <label className="block text-gray-700 font-bold mb-2" htmlFor="email">
-            Endereço
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="email"
-            type="email"
-            placeholder="Endereço estação"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
           />
         </div>
 
@@ -58,6 +100,8 @@ export default function CadastroEstacao() {
             id="latitude"
             type="text"
             placeholder="Lat"
+            value={latitude}
+            onChange={(e) => setLatitude(e.target.value)}
           />
         </div>
 
@@ -71,11 +115,55 @@ export default function CadastroEstacao() {
             id="longitude"
             type="text"
             placeholder="Long"
+            value={longitude}
+            onChange={(e) => setLongitude(e.target.value)}
+          />
+        </div>
+
+        {/* Campo Data de Instalação */}
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2" htmlFor="data_instalacao">
+            Data de Instalação
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="data_instalacao"
+            type="date"
+            value={dataInstalacao}
+            onChange={(e) => setDataInstalacao(e.target.value)}
+          />
+        </div>
+
+        {/* Campo Tipo de Estação */}
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2" htmlFor="tipo_estacao">
+            Tipo de Estação
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="tipo_estacao"
+            type="text"
+            placeholder="Tipo de Estação"
+            value={tipoEstacao}
+            onChange={(e) => setTipoEstacao(e.target.value)}
+          />
+        </div>
+
+        {/* Campo Indicativo Ativa */}
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2" htmlFor="indicativo_ativa">
+            Indicativo Ativa
+          </label>
+          <input
+            type="checkbox"
+            id="indicativo_ativa"
+            checked={indicativoAtiva}
+            onChange={(e) => setIndicativoAtiva(e.target.checked)}
           />
         </div>
 
         {/* Campos de Parâmetros (select) */}
-        {servicos.map((servico, index) => (
+        {estacao.map((servico, index) => (
           <div key={index} className="mb-4">
             <label className="block text-gray-700 font-bold mb-2" htmlFor={`service-${index}`}>
               Parâmetros
@@ -85,13 +173,13 @@ export default function CadastroEstacao() {
               id={`service-${index}`}
               name={`service-${index}`}
               value={servico}
-              onChange={(e) => handleServicoChange(index, e)}
+              onChange={(e) => handleEstacaoChange(index, e)}
             >
               <option value="">Selecione um serviço</option>
               <option value="vento">Vento</option>
-              <option value="umidade">umidade</option>
-              <option value="temeratura">temperatura</option>
-              <option value="pressao">Pressao</option>
+              <option value="umidade">Umidade</option>
+              <option value="temperatura">Temperatura</option>
+              <option value="pressao">Pressão</option>
             </select>
           </div>
         ))}
@@ -102,7 +190,7 @@ export default function CadastroEstacao() {
           onClick={adicionarServico}
           className="btn-adicionar"
         >
-          Adicionar Outro Parametro
+          Adicionar Outro Parâmetro
         </button>
 
         {/* Botão para enviar o formulário */}
@@ -116,25 +204,8 @@ export default function CadastroEstacao() {
         </div>
       </form>
     </div>
+    </>
   );
 }
 
 
-
-
-
-
-{/* <div className="mb-4">
-            <label className="block text-gray-700 font-bold mb-2" htmlFor="parametros">
-                Parametros
-            </label>
-            <select
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="service" name="service">
-                <option value="">Select a service</option>
-                <option value="haircut">Haircut</option>
-                <option value="coloring">Coloring</option>
-                <option value="styling">Styling</option>
-                <option value="facial">Facial</option>
-            </select>
-        </div> */}
